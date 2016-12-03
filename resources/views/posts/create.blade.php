5e71ac832ad5996a35b2b3f052ee0e34c0791d27
ThysBelynda <belynda.thys@devinci.fr>
2016-12-03 12:42:50 +0100

    présentation des pages .blade.php

---
+++ resources/views/posts/create.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                <div class="panel panel-default">
+                    <div class="panel-heading">Créer des évènements</div>
+
+                    <div class="panel-body">
+                        Formulaire création évènement
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
---
+++ resources/views/posts/edit.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                <div class="panel panel-default">
+                    <div class="panel-heading">Modifier un évènement</div>
+
+                    <div class="panel-body">
+                        Formulaire de modification d'un évènement
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
