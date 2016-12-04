f933fe539690eb1c039bf6ebae06802ee8a3f62a
ThysBelynda <belynda.thys@devinci.fr>
2016-12-04 14:17:37 +0100

    Nouveau Seeder créé "PostTableSeeder"

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -21,3 +21,13 @@ $factory->define(App\Models\User::class, function (Faker\Generator $faker) {
         'remember_token' => str_random(10),
     ];
 });
+
+$factory->define(App\Models\Post::class, function (Faker\Generator $faker) {
+    static $password;
+
+    return [
+        'title' => $faker->sentence(10),
+        'content' => $faker->paragraph(4),
+        'user_id' => rand(1, 20),
+    ];
+});
