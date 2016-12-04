caaafbfb2ca1954d68dc1b9745618cdaf219b419
ThysBelynda <belynda.thys@devinci.fr>
2016-12-04 12:14:28 +0100

    J'ai ajouté la classe UsersTableSeeder dans le fichier DatabaseSeeder.php car mes tables ne se remplissaient pas

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,3 +14,18 @@ class DatabaseSeeder extends Seeder
         $this->call(UsersTableSeeder::class);
     }
 }
+
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+
+        factory(\App\Models\User::class, 20)->create();
+    }
+}
