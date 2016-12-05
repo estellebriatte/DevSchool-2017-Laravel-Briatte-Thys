d3002706e00894522ee7d9502da6e9acfebb074d
ThysBelynda <belynda.thys@devinci.fr>
2016-12-05 16:23:16 +0100

    liens pour modifier ou supprimer un article

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -52,7 +52,7 @@ class PostController extends Controller
      * @return \Illuminate\Http\Response
      */
 
-    
+
     public function show($id)
     {
         $post = Post::findOrFail($id);
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -5,10 +5,28 @@
         <div class="row">
             <div class="col-md-8 col-md-offset-2">
                 <div class="panel panel-default">
-                    <div class="panel-heading">Afficher un évènement</div>
+                    <div class="panel-heading">{{ $post->title }}</div>
 
                     <div class="panel-body">
-                        Un évènement
+                        {{$post->content}}
+                        <br>
+
+                        <a href="{{ route('post.edit', $post->id) }}" class="btn btn-info">Modifier</a>
+
+                        <br>
+
+                        {!! Form::model(
+                       $post,
+                       array(
+                       'route' => array('post.destroy', $post->id),
+                       'method' => 'DELETE'))
+                       !!}
+
+                        {!! Form::submit('Supprimer',
+                        ['class' => 'btn btn-danger']) !!}
+
+                        {!! Form::close() !!}
+
                     </div>
                 </div>
             </div>
