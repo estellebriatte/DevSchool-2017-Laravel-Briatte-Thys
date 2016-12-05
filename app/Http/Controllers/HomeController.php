cc790fb096466445e5524bf88eac5832e6e3e3d6
ThysBelynda <belynda.thys@devinci.fr>
2016-12-05 23:22:18 +0100

    Afficher Mes articles dans le profil

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
+use Event;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -23,6 +25,10 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+
+        $posts = Post::orderBy('id', 'desc')->paginate(10);
+
+        return view('home', compact('posts'));
+
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,16 +8,33 @@
                 <div class="panel-heading">Liste des articles que j'ai publiés</div>
 
                 <div class="panel-body">
-                    Afficher ici
+                    @foreach($posts as $post)
+                        <h2>
+                            <a href="{{ route('post.show', $post->id) }}">
+                                {{ $post->title }}
+                            </a>
+                        </h2>
+
+                        <p> {{ $post->content}}</p>
+                    @endforeach
                 </div>
             </div>
         </div>
         <div class="col-md-4">
             <div class="panel panel-default">
                 <div class="panel-heading">Liste des évènements que j'ai créés</div>
+                    <?php /*@foreach($events as $event)
+                        <h2>
+                            <a href="{{ route('post.show', $event->findOrFail(21)) }}">
+                                {{ $event->title }}
+                            </a>
+                        </h2>
+
+                        <p> {{ $event->content}}</p>
+                    @endforeach */?>
 
                 <div class="panel-body">
-                    Afficher ici
+
                 </div>
             </div>
         </div>
