6c1fd0036ade5fbef032787bf7c69fb56cd4343f
ThysBelynda <belynda.thys@devinci.fr>
2016-12-05 15:50:55 +0100

    test

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -51,6 +51,8 @@ class PostController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
+
+    
     public function show($id)
     {
         $post = Post::findOrFail($id);
