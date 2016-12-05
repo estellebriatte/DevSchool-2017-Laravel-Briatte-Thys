9113369491469f3eccff07f9a189977f4a4bb244
Unknown <estelle.briatte@devinci.fr>
2016-12-05 12:06:22 +0100

    creation_events_table

---
+++ database/migrations/2016_12_05_103003_create_events_table.php
@@ -0,0 +1,38 @@
+ph<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEventsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title');
+            $table->date('start');
+            $table->date('end');
+            $table->string('place');
+            $table->integer('price');
+            $table->mediumText('content');
+            $table->integer('user_id')->foreign('user_id')->references('id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('events');
+    }
+}
