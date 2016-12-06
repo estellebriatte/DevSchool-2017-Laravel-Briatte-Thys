b2e6dd76d5e36f4912c6244830de44f8d35c4588
Unknown <estelle.briatte@devinci.fr>
2016-12-06 21:17:19 +0100

    creation admin controller + dossier admin dans views

---
+++ app/Http/Controllers/Admin/EventController.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: SCHNECK_DU_93
+ * Date: 06/12/2016
+ * Time: 21:14
+ */
---
+++ resources/views/admin/index.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-offset-2 col-md-5">
+                <div class="panel panel-default">
+                    <div class="panel-heading">Administration </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
