46bb47f656dc0ae9d2c4c5ed88b6cc3e97a92f0c
ThysBelynda <belynda.thys@devinci.fr>
2016-12-07 17:50:51 +0100

    que admin qui peut modifier ou supprimer

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -6,7 +6,6 @@
             <div class="col-md-8 col-md-offset-2">
                 <div class="panel panel-default">
                     <div class="panel-heading">{{ $post->title }}</div>
-
                     <div class="panel-body">
                         {{$post->content}}
 
@@ -33,26 +32,6 @@
 
                             {!! Form::close() !!}
                         @endif
-
-                        @if(Auth::id() == $post->user_id())
-                            <a href="{{ route('post.edit', $post->id) }}" class="btn btn-group-justified btn-info">Modifier</a>
-
-                            <br>
-
-                            {!! Form::model(
-                           $post,
-                           array(
-                           'route' => array('post.destroy', $post->id),
-                           'method' => 'DELETE'))
-                           !!}
-
-                            {!! Form::submit('Supprimer',
-                            ['class' => 'btn btn-group-justified btn-danger']) !!}
-
-                            {!! Form::close() !!}
-                        @endif
-
-
 
                     </div>
                 </div>
